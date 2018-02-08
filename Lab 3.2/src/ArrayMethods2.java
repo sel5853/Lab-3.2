864614b098d8eb2d7b7d4ed14a007e5a9dd07f9f
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-02-08 09:23:04 -0500

    5

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -3,57 +3,45 @@ public class ArrayMethods2 {
 
 	public static String[] merge(String[] list1, String[] list2)
 	{
-		for(int x = 0; x < list1.length - 1; x++)
+		int x, y ,z = 0;
+		while(z < list1.length + list2.length)
 		{
-			String a = list1[x];
-			String b = list2[x];
-			if(a.compareTo(b) <= 0)
+			if(list1[x] <= list2[y])
 			{
-				mergeSort(list1[x], list2[x]);
+				
+			}
+			
+			if(list1[x] > list2[y])
+			{
+				
 			}
 		}
+		z++;
 	}
 	
 	public static int partition(int[] list)
 	{
 		int y = list[0];
+		int z = 0;
 		for(int x = 0; x < list.length - 1; x++)
 		{
 			if(list[x] <= y)
 			{
-				lowerArrange(list, y, list[x]);
+				
 			}
 			else if(list[x] > y)
 			{
-				upperArrange(list, y, list[x]);
+				
 			}
 			
 			for(int i = 0; i < list.length - 1; i++)
 			{
 				if(list[i] == y)
 				{
-					return i;
+					i = z;
 				}
 			}
 		}
-	}
-	
-	public static void lowerArrange(int[] list, int index1, int index2)
-	{
-		int i = list[index1];
-		list[index1] = list[index2];
-		list[index2] = i;
-	}
-	
-	public static void upperArrange(int[] list, int index1, int index2)
-	{
-		int i = list[index1];
-		list[index1] = list[index2];
-		list[index2] = i;
-	}
-	
-	public static void mergeSort(String x, String y)
-	{
-		
+		return z;
 	}
 }
