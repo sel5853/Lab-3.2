895780f5a4fafca6bdd1c016b37688ea6c24899d
Stanley <sliang5785@bths.edu>
2018-02-09 01:06:08 -0500

    6

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,22 +1,34 @@
 
-public class ArrayMethods2 {
-
+public class ArrayMethods2 
+{
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		int x, y ,z = 0;
+		String[] list3 = new String[list1.length + list2.length];
 		while(z < list1.length + list2.length)
 		{
 			if(list1[x] <= list2[y])
 			{
-				
+				x++;
 			}
 			
 			if(list1[x] > list2[y])
 			{
+				y++;
+			}
+			
+			if()
+			{
 				
 			}
+			
+			if()
+			{
+				
+			}
+			z++;
 		}
-		z++;
+		return list3;
 	}
 	
 	public static int partition(int[] list)
@@ -43,5 +55,34 @@ public class ArrayMethods2 {
 			}
 		}
 		return z;
+	}
+
+	public static void main(String[] args) 
+	{
+		String[] list1 = {"apple","cucumber","microsoft","zorro"};
+		String[] list2 = {"banana","cherry","mahogany","oreos", "pinata"};
+		int[] list = {3,4,2,7,12,22,0,1};
+
+		long start = System.nanoTime();
+		String[] mergeResult = merge(list1, list2);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Test1 took: " + time + " nanoseconds");
+		System.out.println(Arrays.toString(mergeResult));
+		
+		start = System.nanoTime();
+		int pivotFinalPos = partition(list);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Test2 took: " + time + " nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);
+		System.out.println(list);
+	}	
+	
+	public static void swap(int list[], int index1, int index2)
+	{
+		int x = list[index1];
+		list[index1] = list[index2];
+		list[index2] = x;
 	}
 }
